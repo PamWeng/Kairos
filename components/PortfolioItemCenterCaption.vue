56c6bb9529e8a90611015d35df69d1aef5dc9ba5
Pam Weng <quafff@gmail.com>
2025-06-16 15:17:38 +0800

    fix:fix the missing img in sub-page by useAssetUrl()

--- components/PortfolioItemCenterCaption.vue
+++ components/PortfolioItemCenterCaption.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="portfolio portfolio_style--1">
-        <div class="thumb">
-            <img :src="portfolio.image" :alt="portfolio.heading">
-        </div>
-        <div class="port-overlay-info">
-            <div class="hover-action">
-                <h3 class="post-overlay-title">
-                    <nuxt-link :to="`/portfolio/${portfolio.slug}`">{{ portfolio.heading }}</nuxt-link>
-                </h3>
-            </div>
-        </div>
+  <div class="portfolio portfolio_style--1">
+    <div class="thumb">
+      <img :src="useAssetUrl(portfolio.image)" :alt="portfolio.heading" />
     </div>
+    <div class="port-overlay-info">
+      <div class="hover-action">
+        <h3 class="post-overlay-title">
+          <nuxt-link :to="`/portfolio/${portfolio.slug}`">{{
+            portfolio.heading
+          }}</nuxt-link>
+        </h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ['portfolio'],
-    };
+export default {
+  props: ["portfolio"],
+};
 </script>
